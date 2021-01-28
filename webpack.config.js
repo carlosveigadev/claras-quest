1666c9a47f68aefbb10cc8b541707f170a661031
wrakc <carlosveiga1618@gmail.com>
2021-01-28 14:00:44 -0300

    fix errors with older version of copywebpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -25,4 +26,17 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(__dirname, 'build'),
   },
+
+  plugins: [
+    new CopyWebpackPlugin(
+      {
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src/index.html'),
+            to: path.resolve(__dirname, 'build'),
+          },
+        ],
+      },
+    ),
+  ],
 };
