20e05ec859a6d49b99c8f8834081c82afb83eff1
wrack <>
2021-01-29 14:19:07 -0300

    finish tutorial for the rpg game

--- src/js/game.js
+++ src/js/game.js
@@ -1,25 +1,4 @@
 /* eslint-disable no-undef */
-const BootScene = new Phaser.Class({
-
-  Extends: Phaser.Scene,
-
-  initialize:
-
-  function BootScene() {
-    Phaser.Scene.call(this, { key: 'BootScene' });
-  },
-
-  preload() {
-    this.load.spritesheet('player', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
-    this.load.image('dragonblue', 'assets/dragonblue.png');
-    this.load.image('dragonorrange', 'assets/dragonorrange.png');
-  },
-
-  create() {
-    this.scene.start('BattleScene');
-  },
-});
-
 const Unit = new Phaser.Class({
   Extends: Phaser.GameObjects.Sprite,
 
@@ -31,17 +10,36 @@ const Unit = new Phaser.Class({
     this.maxHp = this.hp;
     this.hp = hp;
     this.damage = damage;
+    this.living = true;
+    this.menuItem = null;
+  },
+  setMenuItem(item) {
+    this.menuItem = item;
   },
   attack(target) {
-    target.takeDamage(this.damage);
-    this.scene.events.emit('Message', `${this.type} attacks ${target.type} for ${this.damage} damage`);
+    if (target.living) {
+      target.takeDamage(this.damage);
+      this.scene.events.emit('Message', `${this.type} attacks ${target.type} for ${this.damage} damage`);
+    }
   },
   takeDamage(damage) {
     this.hp -= damage;
     if (this.hp <= 0) {
       this.hp = 0;
-      this.alive = false;
+      this.menuItem.unitKilled();
+      this.living = false;
+      this.visible = false;
+      this.menuItem = null;
     }
+  },
+});
+
+const Enemy = new Phaser.Class({
+  Extends: Unit,
+
+  initialize:
+  function Enemy(scene, x, y, texture, frame, type, hp, damage) {
+    Unit.call(this, scene, x, y, texture, frame, type, hp, damage);
   },
 });
 
@@ -57,17 +55,8 @@ const PlayerCharacter = new Phaser.Class({
   },
 });
 
-const Enemy = new Phaser.Class({
-  Extends: Unit,
-
-  initialize:
-  function Enemy(scene, x, y, texture, frame, type, hp, damage) {
-    Unit.call(this, scene, x, y, texture, frame, type, hp, damage);
-  },
-});
-
-
 const BattleScene = new Phaser.Class({
+
   Extends: Phaser.Scene,
 
   initialize:
@@ -77,7 +66,10 @@ const BattleScene = new Phaser.Class({
   },
   create() {
     this.cameras.main.setBackgroundColor('rgba(0, 200, 0, 0.5)');
-
+    this.startBattle();
+    this.sys.events.on('wake', this.startBattle, this);
+  },
+  startBattle() {
     const warrior = new PlayerCharacter(this, 250, 50, 'player', 1, 'Warrior', 100, 20);
     this.add.existing(warrior);
 
@@ -94,30 +86,58 @@ const BattleScene = new Phaser.Class({
     this.enemies = [dragonblue, dragonOrange];
     this.units = this.heroes.concat(this.enemies);
 
-    this.scene.launch('UIScene');
-
     this.index = -1;
+
+    this.scene.run('UIScene');
   },
   nextTurn() {
-    this.index += 1;
-    if (this.index >= this.units.length) {
-      this.index = 0;
+    if (this.checkEndBattle()) {
+      this.endBattle();
+      return;
     }
-    if (this.units[this.index]) {
-      if (this.units[this.index] instanceof PlayerCharacter) {
-        this.events.emit('PlayerSelect', this.index);
-      } else {
-        const r = Math.floor(Math.random() * this.heroes.length);
-        this.units[this.index].attack(this.heroes[r]);
-        this.time.addEvent({ delay: 3000, callback: this.nextTurn, callbackScope: this });
+    do {
+      this.index += 1;
+      if (this.index >= this.units.length) {
+        this.index = 0;
       }
+    } while (!this.units[this.index].living);
+    if (this.units[this.index] instanceof PlayerCharacter) {
+      this.events.emit('PlayerSelect', this.index);
+    } else {
+      let r;
+      do {
+        r = Math.floor(Math.random() * this.heroes.length);
+      } while (!this.heroes[r].living);
+      this.units[this.index].attack(this.heroes[r]);
+      this.time.addEvent({ delay: 3000, callback: this.nextTurn, callbackScope: this });
     }
+  },
+  checkEndBattle() {
+    let victory = true;
+    for (let i = 0; i < this.enemies.length; i += 1) {
+      if (this.enemies[i].living) victory = false;
+    }
+    let gameOver = true;
+    for (let i = 0; i < this.heroes.length; i += 1) {
+      if (this.heroes[i].living) gameOver = false;
+    }
+    return victory || gameOver;
   },
   receivePlayerSelection(action, target) {
     if (action === 'attack') {
       this.units[this.index].attack(this.enemies[target]);
     }
     this.time.addEvent({ delay: 3000, callback: this.nextTurn, callbackScope: this });
+  },
+  endBattle() {
+    this.heroes.length = 0;
+    this.enemies.length = 0;
+    for (let i = 0; i < this.units.length; i += 1) {
+      this.units[i].destroy();
+    }
+    this.units.length = 0;
+    this.scene.sleep('UIScene');
+    this.scene.switch('WorldScene');
   },
 });
 
@@ -137,6 +157,10 @@ const MenuItem = new Phaser.Class({
   deselect() {
     this.setColor('#ffffff');
   },
+  unitKilled() {
+    this.active = false;
+    this.visible = false;
+  },
 
 });
 
@@ -149,36 +173,48 @@ const Menu = new Phaser.Class({
     Phaser.GameObjects.Container.call(this, scene, x, y);
     this.menuItems = [];
     this.menuItemIndex = 0;
-    this.heroes = heroes;
     this.x = x;
     this.y = y;
+    this.selected = false;
   },
   addMenuItem(unit) {
     const menuItem = new MenuItem(0, this.menuItems.length * 20, unit, this.scene);
     this.menuItems.push(menuItem);
     this.add(menuItem);
+    return menuItem;
   },
   moveSelectionUp() {
     this.menuItems[this.menuItemIndex].deselect();
-    this.menuItemIndex -= 1;
-    if (this.menuItemIndex < 0) this.menuItemIndex = this.menuItems.length - 1;
+    do {
+      this.menuItemIndex -= 1;
+      if (this.menuItemIndex < 0) this.menuItemIndex = this.menuItems.length - 1;
+    } while (!this.menuItems[this.menuItemIndex].active);
     this.menuItems[this.menuItemIndex].select();
   },
   moveSelectionDown() {
     this.menuItems[this.menuItemIndex].deselect();
-    this.menuItemIndex += 1;
-    if (this.menuItemIndex >= this.menuItems.length) this.menuItemIndex = 0;
+    do {
+      this.menuItemIndex += 1;
+      if (this.menuItemIndex >= this.menuItems.length) this.menuItemIndex = 0;
+    } while (!this.menuItems[this.menuItemIndex].active);
     this.menuItems[this.menuItemIndex].select();
   },
   select(index) {
     if (!index) index = 0;
     this.menuItems[this.menuItemIndex].deselect();
     this.menuItemIndex = index;
+    while (!this.menuItems[this.menuItemIndex].active) {
+      this.menuItemIndex += 1;
+      if (this.menuItemIndex >= this.menuItems.length) this.menuItemIndex = 0;
+      if (this.menuItemIndex === index) return;
+    }
     this.menuItems[this.menuItemIndex].select();
+    this.selected = true;
   },
   deselect() {
     this.menuItems[this.menuItemIndex].deselect();
     this.menuItemIndex = 0;
+    this.selected = false;
   },
   confirm() {
   },
@@ -193,8 +229,9 @@ const Menu = new Phaser.Class({
     this.clear();
     for (let i = 0; i < units.length; i += 1) {
       const unit = units[i];
-      this.addMenuItem(unit.type);
+      unit.setMenuItem(this.addMenuItem(unit.type));
     }
+    this.menuItemIndex = 0;
   },
 });
 
@@ -218,7 +255,7 @@ const ActionsMenu = new Phaser.Class({
     this.addMenuItem('Attack');
   },
   confirm() {
-    this.scene.events.emit('SelectEnemies');
+    this.scene.events.emit('SelectedAction');
   },
 
 });
@@ -250,7 +287,7 @@ const Message = new Phaser.Class({
     graphics.strokeRect(-90, -15, 180, 30);
     graphics.fillRect(-90, -15, 180, 30);
     this.text = new Phaser.GameObjects.Text(scene, 0, 0, '', {
-      color: '#ffffff', align: 'center', fontSize: 13, wordWrap: { width: 160, useAdvancedWrap: true },
+      color: '#ffffff', align: 'center', fontSize: 13, wordWrap: { width: 170, useAdvancedWrap: true },
     });
     this.add(this.text);
     this.text.setOrigin(0.5);
@@ -272,7 +309,6 @@ const Message = new Phaser.Class({
     this.visible = false;
   },
 });
-
 
 const UIScene = new Phaser.Class({
 
@@ -309,20 +345,24 @@ const UIScene = new Phaser.Class({
 
     this.battleScene = this.scene.get('BattleScene');
 
-    this.remapHeroes();
-    this.remapEnemies();
-
     this.input.keyboard.on('keydown', this.onKeyInput, this);
 
     this.battleScene.events.on('PlayerSelect', this.onPlayerSelect, this);
 
-    this.events.on('SelectEnemies', this.onSelectEnemies, this);
+    this.events.on('SelectedAction', this.onSelectedAction, this);
 
     this.events.on('Enemy', this.onEnemy, this);
+
+    this.sys.events.on('wake', this.createMenu, this);
 
     this.message = new Message(this, this.battleScene.events);
     this.add.existing(this.message);
 
+    this.createMenu();
+  },
+  createMenu() {
+    this.remapHeroes();
+    this.remapEnemies();
     this.battleScene.nextTurn();
   },
   onEnemy(index) {
@@ -337,7 +377,7 @@ const UIScene = new Phaser.Class({
     this.actionsMenu.select(0);
     this.currentMenu = this.actionsMenu;
   },
-  onSelectEnemies() {
+  onSelectedAction() {
     this.currentMenu = this.enemiesMenu;
     this.enemiesMenu.select(0);
   },
@@ -350,7 +390,7 @@ const UIScene = new Phaser.Class({
     this.enemiesMenu.remap(enemies);
   },
   onKeyInput(event) {
-    if (this.currentMenu) {
+    if (this.currentMenu && this.currentMenu.selected) {
       if (event.code === 'ArrowUp') {
         this.currentMenu.moveSelectionUp();
       } else if (event.code === 'ArrowDown') {
@@ -364,6 +404,151 @@ const UIScene = new Phaser.Class({
   },
 });
 
+const BootScene = new Phaser.Class({
+
+  Extends: Phaser.Scene,
+
+  initialize:
+
+  function BootScene() {
+    Phaser.Scene.call(this, { key: 'BootScene' });
+  },
+
+  preload() {
+    this.load.image('tiles', 'assets/map/spritesheet.png');
+
+    this.load.tilemapTiledJSON('map', 'assets/map/map.json');
+
+    this.load.image('dragonblue', 'assets/dragonblue.png');
+    this.load.image('dragonorrange', 'assets/dragonorrange.png');
+
+    this.load.spritesheet('player', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
+  },
+
+  create() {
+    this.scene.start('WorldScene');
+  },
+});
+
+const WorldScene = new Phaser.Class({
+
+  Extends: Phaser.Scene,
+
+  initialize:
+
+  function WorldScene() {
+    Phaser.Scene.call(this, { key: 'WorldScene' });
+  },
+
+  preload() {
+
+  },
+
+  create() {
+    const map = this.make.tilemap({ key: 'map' });
+
+    const tiles = map.addTilesetImage('spritesheet', 'tiles');
+
+    const grass = map.createStaticLayer('Grass', tiles, 0, 0);
+    const obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0);
+
+    obstacles.setCollisionByExclusion([-1]);
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13] }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13] }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: 'up',
+      frames: this.anims.generateFrameNumbers('player', { frames: [2, 8, 2, 14] }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: 'down',
+      frames: this.anims.generateFrameNumbers('player', { frames: [0, 6, 0, 12] }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.player = this.physics.add.sprite(50, 100, 'player', 6);
+
+    this.physics.world.bounds.width = map.widthInPixels;
+    this.physics.world.bounds.height = map.heightInPixels;
+    this.player.setCollideWorldBounds(true);
+
+    this.physics.add.collider(this.player, obstacles);
+
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.startFollow(this.player);
+    this.cameras.main.roundPixels = true;
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.spawns = this.physics.add.group({ classType: Phaser.GameObjects.Zone });
+    for (let i = 0; i < 30; i += 1) {
+      const x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
+      const y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
+      this.spawns.create(x, y, 20, 20);
+    }
+    this.physics.add.overlap(this.player, this.spawns, this.onMeetEnemy, false, this);
+    this.sys.events.on('wake', this.wake, this);
+  },
+  wake() {
+    this.cursors.left.reset();
+    this.cursors.right.reset();
+    this.cursors.up.reset();
+    this.cursors.down.reset();
+  },
+  onMeetEnemy(player, zone) {
+    zone.x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
+    zone.y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
+
+    this.cameras.main.shake(300);
+
+    this.input.stopPropagation();
+    this.scene.switch('BattleScene');
+  },
+  update(time, delta) {
+    this.player.body.setVelocity(0);
+
+    if (this.cursors.left.isDown) {
+      this.player.body.setVelocityX(-80);
+    } else if (this.cursors.right.isDown) {
+      this.player.body.setVelocityX(80);
+    }
+    if (this.cursors.up.isDown) {
+      this.player.body.setVelocityY(-80);
+    } else if (this.cursors.down.isDown) {
+      this.player.body.setVelocityY(80);
+    }
+
+    if (this.cursors.left.isDown) {
+      this.player.anims.play('left', true);
+      this.player.flipX = true;
+    } else if (this.cursors.right.isDown) {
+      this.player.anims.play('right', true);
+      this.player.flipX = false;
+    } else if (this.cursors.up.isDown) {
+      this.player.anims.play('up', true);
+    } else if (this.cursors.down.isDown) {
+      this.player.anims.play('down', true);
+    } else {
+      this.player.anims.stop();
+    }
+  },
+
+});
+
 const config = {
   type: Phaser.AUTO,
   parent: 'content',
@@ -375,10 +560,15 @@ const config = {
     default: 'arcade',
     arcade: {
       gravity: { y: 0 },
+      debug: false,
     },
   },
-  scene: [BootScene, BattleScene, UIScene],
+  scene: [
+    BootScene,
+    WorldScene,
+    BattleScene,
+    UIScene,
+  ],
 };
-
 const game = new Phaser.Game(config);
 game();
