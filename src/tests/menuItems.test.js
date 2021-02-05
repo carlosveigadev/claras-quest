6dc90e36c96833ecd5997172eee4e1f0d6fb3a9a
wrack <carlosveiga1618@gmail.com>
2021-02-05 14:57:26 -0300

    add more tests

--- src/tests/menuItems.test.js
+++ src/tests/menuItems.test.js
@@ -1,23 +1,39 @@
-import Phaser from 'phaser';
 import menuItems from '../js/menuItems';
 
 describe('Test menu select items.', () => {
   const mockItems = {
-    item: {
-      itemStart: 'Start',
-      itemHowToPlay: 'How to Play',
-      itemSounds: 'Sounds',
-      itemCredits: 'Credits',
-      itemScores: 'Scores',
-    },
-    scene: { start() { return 'test'; } },
-    sound: { setMute() { return 'test'; } },
+    itemStart: { text: 'Start' },
+    itemTuto: { text: 'How to Play' },
+    itemSounds: { text: 'Sounds' },
+    itemCredits: { text: 'Credits' },
+    itemScores: { text: 'Scores' },
+    itemOther: { text: 'Other' },
   };
   test('Menu select must be a function', () => {
     expect(typeof menuItems).toBe('function');
   });
 
-  test('When "Start" is passed to menuItems it should return a function', () => {
-    expect(menuItems('Start', mockItems, mockItems)).toBe('function');
+  test('When "Start" is passed to menuItems it should return a string', () => {
+    expect(menuItems(mockItems.itemStart)).toBe('WorldScene');
+  });
+
+  test('When "Tutorial" is passed to menuItems it should return a string', () => {
+    expect(menuItems(mockItems.itemTuto)).toBe('Tutorial');
+  });
+
+  test('When "Sounds" is passed to menuItems it should return a string', () => {
+    expect(menuItems(mockItems.itemSounds)).toBe('Sounds');
+  });
+
+  test('When "Credits" is passed to menuItems it should return a string', () => {
+    expect(menuItems(mockItems.itemCredits)).toBe('Credits');
+  });
+
+  test('When "Scores" is passed to menuItems it should return a string', () => {
+    expect(menuItems(mockItems.itemScores)).toBe('Scores');
+  });
+
+  test('When any other type of string is passed to menuItems it should return false', () => {
+    expect(menuItems(mockItems.itemOther)).toBe(false);
   });
 });
